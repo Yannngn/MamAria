119e92e1cbc1dc39cf70bc8011147ee8aa4845c9
Yann Nóbrega <yann.nicholas@gmail.com>
2021-10-03 18:30:40 -0300

    edit cropper

--- cropper.py
+++ cropper.py
@@ -2,10 +2,15 @@ import os
 from PIL import Image
 
 DIR = "C:/Users/Yann/Documents/GitHub/PyTorch_Seg/data/mask/"
+PHA = "C:/Users/Yann/Documents/GitHub/PyTorch_Seg/data/phantom/"
+
+phant = Image.open(PHA + os.listdir(PHA)[0])
+print(os.listdir(PHA)[0])
+img_height = phant.size[1]
 
 for root, dirs, files in os.walk(DIR):
-    for filename in files:
-        if filename[-4:] == ".png":
-            mask = Image.open(os.path.join(DIR, filename))
+    for file in files:
+        if file[-4:] == ".png":
+            mask = Image.open(DIR + file)
             w, h = mask.size
-            mask.crop((0, h-256, w, h)).save(os.path.join(DIR, filename))
+            mask.crop((0, img_height-256, w, h)).save(DIR + file)
--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from tqdm import tqdm
 import torch.nn as nn
 import torch.optim as optim
 from model import UNET
-from soft_IoU_loss import SoftIoULoss
 from utils import (
     load_checkpoint,
     save_checkpoint,
